5716cb3c65cc9822328f472b824105424ca512c5
Jusan Magno <jusanmagno@gmail.com>
2019-11-26 18:31:06 +0000

    atualização

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const Express = require('express')
 const app = new Express()
+var axios = require('axios')
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended : false }));
+var token = ''
 
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + "/views/index.html");
+    res.sendFile(__dirname + "README.md");
 });
 
-app.post('/send-profile/:name', function(req, res){
+app.get('/send-profile/:name', function(req, res){
+    var search = req.params.name;
+    
+    axios.get('https://api.instagram.com/v1/' + search + '/self/?access_token=' + token).than(function(params) {
+        
+    })
 
 });
